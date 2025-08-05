3d11dd9f57699a0b6410a61d9ee8cd6e6bc51eea
Eduardo Kraus <kraus@eduardokraus.com>
2025-08-05 17:55:59 -0300

    V 1.2.7

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2025080500;
+$plugin->version = 2025080501;
 $plugin->requires = 2021041900;
-$plugin->release = "1.2.6";
+$plugin->release = "1.2.7";
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = "local_backupftp";
