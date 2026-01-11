82432c60f8006abb0909deedb7f14b270bafaa38
Eduardo Kraus <kraus@eduardokraus.com>
2026-01-11 20:50:27 -0300

    Whitespace found at end of line

--- classes/server/ftp.php
+++ classes/server/ftp.php
@@ -46,24 +46,24 @@ class ftp {
      * @throws Exception
      */
     public function connect(array $logs = []): array {
-        $ftpurl = trim(get_config('local_backupftp', 
+        $ftpurl = trim(get_config('local_backupftp',
             'ftpurl'));
-        $ftppasv = get_config('local_backupftp', 
+        $ftppasv = get_config('local_backupftp',
             'ftppasv');
-        $ftpusername = get_config('local_backupftp', 
+        $ftpusername = get_config('local_backupftp',
             'ftpusername');
-        $ftppassword = get_config('local_backupftp', 
+        $ftppassword = get_config('local_backupftp',
             'ftppassword');
 
         if ($ftpurl === '') {
-            $logs[] = get_string('ftp_error_connecting', 
+            $logs[] = get_string('ftp_error_connecting',
             'local_backupftp');
             return $logs;
         }
 
         $parsed = $this->parse_ftp_url($ftpurl);
         if (empty($parsed['host'])) {
-            $logs[] = get_string('ftp_error_connecting', 
+            $logs[] = get_string('ftp_error_connecting',
             'local_backupftp');
             return $logs;
         }
@@ -79,13 +79,13 @@ class ftp {
         }
 
         if (empty($this->connid)) {
-            $logs[] = get_string('ftp_error_connecting', 
+            $logs[] = get_string('ftp_error_connecting',
             'local_backupftp');
             return $logs;
         }
 
         if (!@ftp_login($this->connid, $ftpusername, $ftppassword)) {
-            $logs[] = get_string('ftp_error_login', 
+            $logs[] = get_string('ftp_error_login',
             'local_backupftp', ['username' => $ftpusername, 'url' => $ftpurl]);
             $this->connid = null;
             return $logs;
@@ -125,7 +125,7 @@ class ftp {
 
         $url = @parse_url($candidate);
         if (!is_array($url)) {
-            return ['host' => '', 
+            return ['host' => '',
             'port' => 21, 'ftps' => false];
         }
 
@@ -154,24 +154,24 @@ class ftp {
 
         $kb = $bytes / 1000;
         if ($kb < 1000) {
-            return self::remove_zero(number_format($kb, 1, ',', 
+            return self::remove_zero(number_format($kb, 1, ',',
             '.') . ' KB', 1);
         }
 
         $mb = $kb / 1000;
         if ($mb < 1000) {
-            return self::remove_zero(number_format($mb, 1, ',', 
+            return self::remove_zero(number_format($mb, 1, ',',
             '.') . ' MB', 1);
         }
 
         $gb = $mb / 1000;
         if ($gb < 1000) {
-            return self::remove_zero(number_format($gb, 2, ',', 
+            return self::remove_zero(number_format($gb, 2, ',',
             '.') . ' GB', 2);
         }
 
         $tb = $gb / 1000;
-        return self::remove_zero(number_format($tb, 3, ',', 
+        return self::remove_zero(number_format($tb, 3, ',',
             '.') . ' TB', 3);
     }
 
@@ -184,13 +184,13 @@ class ftp {
      */
     private static function remove_zero(string $text, int $count): string {
         if ($count === 3) {
-            return str_replace(',000', 
+            return str_replace(',000',
             '', $text);
         } else if ($count === 2) {
-            return str_replace(',00', 
+            return str_replace(',00',
             '', $text);
         }
-        return str_replace(',0', 
+        return str_replace(',0',
             '', $text);
     }
 
@@ -202,69 +202,69 @@ class ftp {
      */
     public static function remove_accents(string $string): string {
         $map = [
-            'À' => 'A', 
-            'Á' => 'A', 
-            'Â' => 'A', 
-            'Ã' => 'A', 
-            'Ä' => 'A', 
-            'Å' => 'A', 
-            'Æ' => 'AE', 
-            'Ç' => 'C', 
-            'È' => 'E', 
+            'À' => 'A',
+            'Á' => 'A',
+            'Â' => 'A',
+            'Ã' => 'A',
+            'Ä' => 'A',
+            'Å' => 'A',
+            'Æ' => 'AE',
+            'Ç' => 'C',
+            'È' => 'E',
             'É' => 'E',
-            'Ê' => 'E', 
-            'Ë' => 'E', 
-            'Ì' => 'I', 
-            'Í' => 'I', 
-            'Î' => 'I', 
-            'Ï' => 'I', 
-            'Ð' => 'D', 
-            'Ñ' => 'N', 
-            'Ò' => 'O', 
+            'Ê' => 'E',
+            'Ë' => 'E',
+            'Ì' => 'I',
+            'Í' => 'I',
+            'Î' => 'I',
+            'Ï' => 'I',
+            'Ð' => 'D',
+            'Ñ' => 'N',
+            'Ò' => 'O',
             'Ó' => 'O',
-            'Ô' => 'O', 
-            'Õ' => 'O', 
-            'Ö' => 'O', 
-            'Ø' => 'O', 
-            'Ù' => 'U', 
-            'Ú' => 'U', 
-            'Û' => 'U', 
-            'Ü' => 'U', 
-            'Ý' => 'Y', 
+            'Ô' => 'O',
+            'Õ' => 'O',
+            'Ö' => 'O',
+            'Ø' => 'O',
+            'Ù' => 'U',
+            'Ú' => 'U',
+            'Û' => 'U',
+            'Ü' => 'U',
+            'Ý' => 'Y',
             'Þ' => 'TH',
-            'ß' => 'ss', 
-            'à' => 'a', 
-            'á' => 'a', 
-            'â' => 'a', 
-            'ã' => 'a', 
-            'ä' => 'a', 
-            'å' => 'a', 
-            'æ' => 'ae', 
+            'ß' => 'ss',
+            'à' => 'a',
+            'á' => 'a',
+            'â' => 'a',
+            'ã' => 'a',
+            'ä' => 'a',
+            'å' => 'a',
+            'æ' => 'ae',
             'ç' => 'c',
-            'è' => 'e', 
-            'é' => 'e', 
-            'ê' => 'e', 
-            'ë' => 'e', 
-            'ì' => 'i', 
-            'í' => 'i', 
-            'î' => 'i', 
-            'ï' => 'i', 
-            'ð' => 'd', 
+            'è' => 'e',
+            'é' => 'e',
+            'ê' => 'e',
+            'ë' => 'e',
+            'ì' => 'i',
+            'í' => 'i',
+            'î' => 'i',
+            'ï' => 'i',
+            'ð' => 'd',
             'ñ' => 'n',
-            'ò' => 'o', 
-            'ó' => 'o', 
-            'ô' => 'o', 
-            'õ' => 'o', 
-            'ö' => 'o', 
-            'ø' => 'o', 
-            'ù' => 'u', 
-            'ú' => 'u', 
-            'û' => 'u', 
+            'ò' => 'o',
+            'ó' => 'o',
+            'ô' => 'o',
+            'õ' => 'o',
+            'ö' => 'o',
+            'ø' => 'o',
+            'ù' => 'u',
+            'ú' => 'u',
+            'û' => 'u',
             'ü' => 'u',
-            'ý' => 'y', 
-            'þ' => 'th', 
-            'ÿ' => 'y', 
-            'ª' => 'a', 
+            'ý' => 'y',
+            'þ' => 'th',
+            'ÿ' => 'y',
+            'ª' => 'a',
             'º' => 'o',
         ];
 
@@ -272,7 +272,7 @@ class ftp {
         $string = strtr($string, $map);
 
         // Keep only safe characters.
-        $string = preg_replace('/[^A-Za-z0-9\-_\. ]/', 
+        $string = preg_replace('/[^A-Za-z0-9\-_\. ]/',
             '', $string);
         $string = trim($string);
 
