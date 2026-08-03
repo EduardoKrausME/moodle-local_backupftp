79411c5034d856b2d30700ae7c50c8b71edd29d0
Eduardo Kraus <kraus@eduardokraus.com>
2026-08-03 12:11:56 -0300

    2 expected arguments

--- report-backup.php
+++ report-backup.php
@@ -38,7 +38,7 @@ $PAGE->set_pagelayout("admin");
 $PAGE->set_title(get_string('backup_report', 'local_backupftp'));
 $PAGE->set_heading(get_string('backup_report', 'local_backupftp'));
 $PAGE->requires->js_call_amd('local_backupftp/confirmation', 'init');
-$PAGE->requires->strings_for_js(['confirmation', 'yes', 'no']);
+$PAGE->requires->strings_for_js(['confirmation', 'yes', 'no'], 'moodle');
 
 require_login();
 require_capability('local/backupftp:manage', $context);
